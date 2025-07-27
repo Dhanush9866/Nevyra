6f6e0314abf4244db18c5adf1f5c1a1572338e01
Suand9704 <sunandvemavarapu@gmail.com>
2025-07-27 22:43:07 +0530

    initialcommit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,5 +8,6 @@ router.use("/cart", require("./cart"));
 router.use("/orders", require("./orders"));
 router.use("/users", require("./users"));
 router.use("/admins", require("./admins"));
+router.use("/payments", require("./payments"));
 
 module.exports = router;
