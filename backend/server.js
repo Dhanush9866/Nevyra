14861496203c1b76c662979e4d979bcab6e840b7
Hosanna-Mosa <hosannamosa4190@gmail.com>
2025-10-15 12:29:12 +0530

    otp

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
     'https://nevyrafron.onrender.com',
     'https://nevyrafrontend.onrender.com',
     'https://nevyra.onrender.com',
-    'http://localhost:8000', 
+    'http://localhost:8080', 
     'http://localhost:8081',// Your frontend development server
     'http://localhost:3000',
     'http://localhost:5173',
