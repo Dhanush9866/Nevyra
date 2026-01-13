83a979b0c0fad7c1a1693599c3b03f06053f72b0
Hosanna-Mosa <Hosannamosa4190@gmail.com>
2026-01-13 12:53:22 +0530

    Update api.ts

--- seller/src/lib/api.ts
+++ seller/src/lib/api.ts
@@ -1,7 +1,11 @@
 
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000/api/v1";
+
+
+//export const API_BASE_URL = "http://localhost:8000/api/v1";
+export const API_BASE_URL = "https://api.zythova.com/api/v1";
+
 
 const api = axios.create({
     baseURL: API_BASE_URL,
